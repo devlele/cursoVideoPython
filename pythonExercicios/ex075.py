963c398c7b5a19f5e7d76ccdf227d2e67b147a64
Alefe <alefefreitassantos@gmail.com>
2025-02-18 00:33:03 -0300

    Ultimos exericios sobre tupla

--- pythonExercicios/ex075.py
+++ pythonExercicios/ex075.py
@@ -1,6 +1,6 @@
 valores = tuple(int(input('Digite o valor desejado: ')) for _ in range(4)) #o '_' é utilizado por ser uma variavel descartavel, ou seja não será usada dentro do loop
 cont9 = 0
-valor3 = f'O valor 3 apareceu na {valores.index(3) + 1}ª posição'
+
 pares = []
 for i in valores:
     if i == 9:
@@ -9,9 +9,13 @@ for i in valores:
     if i % 2 == 0:
         pares.append(i)
     
-    if valores.index(3) == False:
+    if 3 in valores:
+        valor3 = f'O valor 3 apareceu na {valores.index(3) + 1}ª posição'
+    else:
         valor3 = 'O valor três não foi digitado em nenhuma posição'
+        
 
 print(f'''O valor 9 apareceu {cont9} vezez
 {valor3}
 Os valores pares digitados foram {pares}''')
+
